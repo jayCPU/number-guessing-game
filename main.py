e01bc5c591fb5e0a452962a19e6436b035f981e9
Andrew C <sirjayden1025@gmail.com>
2024-03-31 17:53:10 -0700

    Update main.py

--- main.py
+++ main.py
@@ -8,21 +8,24 @@ upper = int(input("Input upper bound: "))
 #number to guess
 x = random.randint(lower, upper)
 
-#amount of guesses
+#amount of guesses you get using log
 maxtries = round(math.log(upper - lower + 1, 2))
 print("You only have", maxtries,"guesses! Good luck!")
 
+#init tries int
 tries = 0
 
 while tries < maxtries:
     tries += 1
-    
+
     guess = int(input("Guess the number: "))
     
     if guess == x:
         print("Congratulations, that's the correct number!")
         
+        #breaks the loop when guessed
         break
+    
     elif guess < x:
         print("Too low")
     elif guess > x:
